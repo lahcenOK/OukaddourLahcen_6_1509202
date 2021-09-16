b2cdaaee17bdf941cf780d0807dea1b8f288b083
lawsen@hotmail.fr <83468136+lahcenOK@users.noreply.github.com>
2021-09-16 10:48:09 +0200

    Inscription/ connexion  utilisateur

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,28 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
-const userRoutes = require('./routes/user');
+
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: ' Requête reçue !' }); 
-});
+const userRoutes = require('./routes/user');
+
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+ });
 
 mongoose.connect('mongodb+srv://jsuser:Juser@cluster0.vihry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
+{ useNewUrlParser: true,
+   useUnifiedTopology: true })
+   .then(() => console.log('Connexion à MongoDB réussie !'))
+   .catch(() => console.log('Connexion à MongoDB échouée !'));
+   
+   
+   app.use((req, res) => {
+   res.json({ message: ' Requête reçue !' }); 
+   });
 
 app.use('/api/auth', userRoutes);
 module.exports = app;
