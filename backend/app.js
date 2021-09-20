2d18b42661cbbcdf257d2122985e3d2c56b6c712
lawsen@hotmail.fr <83468136+lahcenOK@users.noreply.github.com>
2021-09-20 13:38:46 +0200

    ajout Route & Controlleur sauce

--- backend/app.js
+++ backend/app.js
@@ -5,23 +5,28 @@ const mongoose = require('mongoose');
 const app = express();
 
 const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 
-
+// CORS
 app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
    next();
  });
-
-mongoose.connect('mongodb+srv://jsuser:Juser@cluster0.vihry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+// BDD
+mongoose.connect('mongodb+srv://lahcen:Lahcen71@cluster0.vihry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 { useNewUrlParser: true,
    useUnifiedTopology: true })
    .then(() => console.log('Connexion à MongoDB réussie !'))
    .catch(() => console.log('Connexion à MongoDB échouée !'));
   
-  app.use(express.json());
-   
 
+  app.use(express.json());
+
+  
+
+
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 module.exports = app;
---
+++ backend/routes/sauce.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const sauceCtrl = require('../controllers/sauce');
+
+router.get('/', sauceCtrl.getAllSauce);
+router.post('/', sauceCtrl.createSauce);
+router.get('/:id', sauceCtrl.getOneSauce);
+router.put('/:id', sauceCtrl.modifySauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
+
+module.exports = router;
