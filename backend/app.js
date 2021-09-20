812523d3a670454775ce5571c8988d65694f1a6b
lawsen@hotmail.fr <83468136+lahcenOK@users.noreply.github.com>
2021-09-20 17:45:11 +0200

    Modification des routes sauce pour prendre en compte les fichiers

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-
-const app = express();
-
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
 
-// CORS
-app.use((req, res, next) => {
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-   next();
- });
+const path = require('path');
+
+const app = express();
 // BDD
 mongoose.connect('mongodb+srv://lahcen:Lahcen71@cluster0.vihry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 { useNewUrlParser: true,
-   useUnifiedTopology: true })
-   .then(() => console.log('Connexion à MongoDB réussie !'))
-   .catch(() => console.log('Connexion à MongoDB échouée !'));
+  useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+    
+  // CORS
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(express.json());
   
-
-  app.use(express.json());
-
-  
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,35 +1,62 @@
+// controllers/sauce.js
 const Sauce = require('../models/sauce');
 
-exports.getAllSauce = (req, res, next) => {
-    Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
+//créer une sauce
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
     .catch(error => res.status(400).json({ error }));
 };
-
+// afficher une seule sauce
 exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }));
+  Sauce.findOne({
+    _id: req.params.id
+  }).then(
+    (sauce) => {
+      res.status(200).json(sauce);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
 };
-
-exports.createSauce = (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-    ...req.body
-    });
-    sauce.save()
-        .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-        .catch(error => res.status(400).json({ error }));
-};
-
+// Modifier une sauce
 exports.modifySauce = (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce modifiée!' }))
-        .catch(error => res.status(400).json({ error }));
+  const sauceObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
+    .catch(error => res.status(400).json({ error }));
 };
-
+// Supprimer une sauce
 exports.deleteSauce = (req, res, next) => {
     Sauce.deleteOne({ _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
       .catch(error => res.status(400).json({ error }));
+};
+// Afficher toutes les sauces
+exports.getAllSauce = (req, res, next) => {
+  Sauce.find().then(
+    (sauces) => {
+      res.status(200).json(sauces);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
 };
