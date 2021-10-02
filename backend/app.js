72849d64f71afda00c4740d595718b525c9c95da
oukaddour <lawsen@hotmail.fr>
2021-10-02 09:44:59 +0200

    Ajout d'une variable d'environnement type

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_DB,
 
 // CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8081');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
